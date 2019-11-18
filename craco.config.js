d71763a3f53aa9a743d191b599c202a480aacd45
Stanislav Sysoev <d4rkr00t@gmail.com>
2019-11-19 10:39:53 +1100

    Store multiple stats.json files

--- craco.config.js
+++ craco.config.js
@@ -6,9 +6,27 @@ module.exports = {
     plugins: [
       ...when(
         Boolean(process.env.ANALYZE),
-        () => [new BundleAnalyzerPlugin()],
+        () => [
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static",
+            generateStatsFile: true,
+            statsFilename: "stats." + Date.now() + ".json"
+          })
+        ],
         []
       )
-    ]
+    ],
+    stats: {
+      source: false
+    },
+    alias: {
+      "@atlaskit/media-editor": require.resolve("./src/empty-module.js"),
+      // "@atlaskit/media-picker": require.resolve("./src/empty-module.js"),
+      // "@atlaskit/media-viewer": require.resolve("./src/empty-module.js"),
+      // "@atlaskit/select": require.resolve("./src/empty-module.js"),
+      "styled-components": require.resolve("./src/empty-module.js"),
+      "react-dom": require.resolve("./src/empty-module.js"),
+      react: require.resolve("./src/empty-module.js")
+    }
   }
 };
